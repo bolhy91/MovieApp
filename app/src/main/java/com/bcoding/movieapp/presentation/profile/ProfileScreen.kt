5ea2041fa809015c36595602217ee5cfe6609a48
Bolivar Cortes <bcortes@compilersofts.com>
2021-12-15 02:47:48 -0500

    Modificando profile & ticket screem

--- app/src/main/java/com/bcoding/movieapp/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/bcoding/movieapp/presentation/profile/ProfileScreen.kt
@@ -1,14 +1,18 @@
 package com.bcoding.movieapp.presentation.profile
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ProfileScreen() {
-    Column() {
-        Text(text = "Profile Screen")
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+        Text(text = "Profile Screen", style = MaterialTheme.typography.h1)
     }
 }
 
--- app/src/main/java/com/bcoding/movieapp/presentation/ticket/TicketScreen.kt
+++ app/src/main/java/com/bcoding/movieapp/presentation/ticket/TicketScreen.kt
@@ -1,14 +1,18 @@
 package com.bcoding.movieapp.presentation.ticket
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun TicketScreen() {
-    Column() {
-        Text(text = "Ticket Screen")
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+        Text(text = "Ticket Screen", style = MaterialTheme.typography.h1)
     }
 }
 
